7a010be2cd26c603929be91abe836021f6babc8d
plotchy <plotchy@tutanota.com>
2024-06-12 13:11:27 -0400

    fix: graph render race-condition fixed

--- site/src/components/GraphViewer.js
+++ site/src/components/GraphViewer.js
@@ -5,6 +5,8 @@ import Panzoom from '@panzoom/panzoom';
 const GraphViewer = ({ graph, containerId, onNodeHighlight, diffHighlights, sourceHoverPos }) => {
   const containerRef = useRef(null);
   const lazySortedLocsRef = useRef(null);
+  const panzoomRef = useRef(null);
+  const previousDiffHighlights = useRef([]);
 
   const highlightNodes = (elements, lineages, allNodes) => {
 
@@ -103,127 +105,132 @@ const GraphViewer = ({ graph, containerId, onNodeHighlight, diffHighlights, sour
     }
   };
 
-  useEffect(() => {
-    lazySortedLocsRef.current = null;
-
-    const renderMermaid = async () => {
-      const container = containerRef.current;
-      container.innerHTML = ''; // Clear previous content
-      let modifiedGraph = graph;
-      if (diffHighlights.length > 0) {
-      // Iter over diffHighlights and search and replace for each substr
-        diffHighlights.forEach(highlight => {
-          const [style, position] = highlight.split(' ');
-          const regex = new RegExp(`(${style} ${position})`, 'g');
-          modifiedGraph = modifiedGraph.replace(regex, `$1 fill:#b8a75a,`);
-        });
-      }
-      mermaid.initialize({
-        maxTextSize: 1000000
+  const renderMermaid = async () => {
+    const container = containerRef.current;
+    container.innerHTML = ''; // Clear previous content
+    let modifiedGraph = graph;
+    if (diffHighlights.length > 0) {
+      diffHighlights.forEach(highlight => {
+        const [style, position] = highlight.split(' ');
+        const regex = new RegExp(`(${style} ${position})`, 'g');
+        modifiedGraph = modifiedGraph.replace(regex, `$1 fill:#b8a75a,`);
       });
+    }
+    mermaid.initialize({
+      maxTextSize: 1000000
+    });
 
-      try {
-        const { svg } = await mermaid.render('mermaid', modifiedGraph);
-        const element = document.createElement('div');
-        element.innerHTML = svg;
-        element.className = 'graph-content';
-        container.appendChild(element);
+    try {
+      const { svg } = await mermaid.render('mermaid', modifiedGraph);
+      const element = document.createElement('div');
+      element.innerHTML = svg;
+      element.className = 'graph-content';
+      container.appendChild(element);
 
-        // Initialize panzoom on the rendered element
-        const panzoom = Panzoom(element, {
+      // Initialize panzoom only once
+      if (!panzoomRef.current) {
+        panzoomRef.current = Panzoom(element, {
           maxScale: 30,
           step: 0.07,
         });
-        element.parentElement.addEventListener('wheel', panzoom.zoomWithWheel);
-        // Highlighting logic
-        const allSubgraphs = Array.from(document.querySelectorAll(".subgraph:not(.node)"));
-        allSubgraphs.map((subgraph) => {
-          let label = subgraph.querySelector("*> .nodeLabel");
-          let description = label.innerHTML;
-          let color = "#" + description.split("bgcolor_")[1];
-          Array.from(subgraph.childNodes).map((node) => {
-            if (Array.from(node.classList).includes("node")) {
-              node.style.fill = color;
-            }
-          });
-          if (description) { // Ensure description is not empty
-            subgraph.classList.add(description);
-          }
-          label.innerHTML = "";
-        });
-
-        Array.from(document.querySelectorAll(".path")).map((path) => {
-          path.setAttribute("style", "stroke: orange;");
-        });
-
-        const allNodes = Array.from(document.querySelectorAll('svg .node:not(.subgraph)'));
-        const lineages = new Map();
-
-        // Add event listeners
-        allNodes.forEach(function(el) {
-          el.addEventListener("mouseenter", (event) => {
-            // Find the class that matches the pattern loc_0_108_113
-            const locClass = [...el.classList].find(cls => cls.match(/loc_\d+_\d+_\d+/));
-            let loc = null;
-            let forks = [];
-
-            if (locClass) {
-              const locMatch = locClass.match(/loc_(\d+)_(\d+)_(\d+)/);
-              // console.log(`entered ${locMatch[1]} ${locMatch[2]} ${locMatch[3]}`);
-              loc = {
-                file_number: locMatch[1],
-                start: locMatch[2],
-                end: locMatch[3]
-              };
-            }
-
-            const forksClasses = [...el.classList].filter(cls => cls.match(/fork_\d+_\d+_\d+_(true|false)$/));
-            forksClasses.forEach(forksClass => {
-              const forkMatch = forksClass.match(/fork_(\d+)_(\d+)_(\d+)_(true|false)$/);
-              // console.log(`entered ${forkMatch[1]} ${forkMatch[2]} ${forkMatch[3]} ${forkMatch[4]}`);
-              forks.push({
-                file_number: forkMatch[1],
-                start: forkMatch[2],
-                end: forkMatch[3],
-                edge: forkMatch[4] === "true"
-              });
-            });
-
-            onNodeHighlight(loc, forks);
-            highlightNodes(el, lineages, allNodes);
-          });
-
-          el.addEventListener("mouseleave", (event) => {
-            // Call onNodeHighlight with null
-            // console.log("mouseleave");
-            onNodeHighlight(null, []);
-
-            let maybeLineage = lineages.get(el);
-            if (maybeLineage) {
-              let lineage = maybeLineage[0];
-              let graphs = maybeLineage[1];
-              allNodes.map((node) => {
-                if (lineage.has(node)) {
-                } else {
-                  node.removeAttribute("style");
-                }
-              });
-            }
-          });
-        });
-        
-      } catch (error) {
-        console.error(error);
+        element.parentElement.addEventListener('wheel', panzoomRef.current.zoomWithWheel);
       }
-    };
 
-    if (graph) {
-      renderMermaid();
+      // Your node highlighting and event listener logic here
+      // Highlighting logic
+      const allSubgraphs = Array.from(document.querySelectorAll(".subgraph:not(.node)"));
+      allSubgraphs.map((subgraph) => {
+        let label = subgraph.querySelector("*> .nodeLabel");
+        let description = label.innerHTML;
+        let color = "#" + description.split("bgcolor_")[1];
+        Array.from(subgraph.childNodes).map((node) => {
+          if (Array.from(node.classList).includes("node")) {
+            node.style.fill = color;
+          }
+        });
+        if (description) { // Ensure description is not empty
+          subgraph.classList.add(description);
+        }
+        label.innerHTML = "";
+      });
+
+      Array.from(document.querySelectorAll(".path")).map((path) => {
+        path.setAttribute("style", "stroke: orange;");
+      });
+
+      const allNodes = Array.from(document.querySelectorAll('svg .node:not(.subgraph)'));
+      const lineages = new Map();
+
+      // Add event listeners
+      allNodes.forEach(function(el) {
+        el.addEventListener("mouseenter", (event) => {
+          // Find the class that matches the pattern loc_0_108_113
+          const locClass = [...el.classList].find(cls => cls.match(/loc_\d+_\d+_\d+/));
+          let loc = null;
+          let forks = [];
+
+          if (locClass) {
+            const locMatch = locClass.match(/loc_(\d+)_(\d+)_(\d+)/);
+            // console.log(`entered ${locMatch[1]} ${locMatch[2]} ${locMatch[3]}`);
+            loc = {
+              file_number: locMatch[1],
+              start: locMatch[2],
+              end: locMatch[3]
+            };
+          }
+
+          const forksClasses = [...el.classList].filter(cls => cls.match(/fork_\d+_\d+_\d+_(true|false)$/));
+          forksClasses.forEach(forksClass => {
+            const forkMatch = forksClass.match(/fork_(\d+)_(\d+)_(\d+)_(true|false)$/);
+            // console.log(`entered ${forkMatch[1]} ${forkMatch[2]} ${forkMatch[3]} ${forkMatch[4]}`);
+            forks.push({
+              file_number: forkMatch[1],
+              start: forkMatch[2],
+              end: forkMatch[3],
+              edge: forkMatch[4] === "true"
+            });
+          });
+
+          onNodeHighlight(loc, forks);
+          highlightNodes(el, lineages, allNodes);
+        });
+
+        el.addEventListener("mouseleave", (event) => {
+          // Call onNodeHighlight with null
+          // console.log("mouseleave");
+          onNodeHighlight(null, []);
+
+          let maybeLineage = lineages.get(el);
+          if (maybeLineage) {
+            let lineage = maybeLineage[0];
+            let graphs = maybeLineage[1];
+            allNodes.map((node) => {
+              if (lineage.has(node)) {
+              } else {
+                node.removeAttribute("style");
+              }
+            });
+          }
+        });
+      });
+
+    } catch (error) {
+      console.error(error);
     }
-  }, [diffHighlights]);
+  };
 
   useEffect(() => {
-    // Handle sourceHoverPos
+    lazySortedLocsRef.current = null;
+
+    if (JSON.stringify(diffHighlights) !== JSON.stringify(previousDiffHighlights.current)) {
+      previousDiffHighlights.current = diffHighlights;
+      if (graph) {
+        renderMermaid();
+      }
+    }
+  }, [diffHighlights, graph]);
+
+  useEffect(() => {
     if (sourceHoverPos) {
       const { file_number, pos } = sourceHoverPos;
       // find the node that has the matching file_number and the closest start. on ties, pick the smallest start-end range
